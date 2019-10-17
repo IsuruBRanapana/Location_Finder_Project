14db1c95a56ee2b89c4ae2c9e70d062e3c66f69f
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-10-17 22:55:04 +0530

    Updated SignUp

--- ProjectLocation/location_finder/lib/app_screens/signup.dart
+++ ProjectLocation/location_finder/lib/app_screens/signup.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class SignUp extends StatefulWidget{
+class SignUp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
@@ -8,9 +8,20 @@ class SignUp extends StatefulWidget{
   }
 }
 
-class SignUpState extends State<SignUp>{
+class SignUpState extends State<SignUp> {
+  var _formKey = GlobalKey<FormState>();
+  double _minimumPadding=5.0;
+
+  TextEditingController first_name_controller=TextEditingController();
+  TextEditingController last_name_controller=TextEditingController();
+  TextEditingController email_controller=TextEditingController();
+  TextEditingController phone_number_controller=TextEditingController();
+  TextEditingController password_controller=TextEditingController();
+  TextEditingController con_password_controller=TextEditingController();
+
   @override
   Widget build(BuildContext context) {
+
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(
@@ -23,13 +34,44 @@ class SignUpState extends State<SignUp>{
     );
   }
 
-  Widget signUpForm(){
-    return Padding(
-      padding: EdgeInsets.all(15.0),
-      child: ListView(
-        children: <Widget>[
-          
-        ],
+  Widget signUpForm() {
+    TextStyle textStyle = Theme.of(context).textTheme.title;
+    return Form(
+      key: _formKey,
+      child: Padding(
+        padding: EdgeInsets.all(_minimumPadding*3),
+        child: ListView(
+          children: <Widget>[
+
+            //First Name
+            Padding(
+              padding: EdgeInsets.only(top: _minimumPadding,bottom: _minimumPadding),
+              child: TextFormField(
+                controller: first_name_controller,
+                validator: (String value) {
+                  if (value.isEmpty) {
+                    return "Enter the First Name";
+                  }
+                  return null;
+                },
+                keyboardType: TextInputType.text,
+                style: textStyle,
+                decoration: InputDecoration(
+                    labelText: "Rate of Interest",
+                    labelStyle: textStyle,
+                    errorStyle: TextStyle(
+                      color: Colors.yellowAccent,
+                      fontSize: 15.0,
+                    ),
+                    hintText: "In percent",
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5.0),
+                    ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
