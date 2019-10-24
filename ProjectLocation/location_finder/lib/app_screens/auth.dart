f2f6d34b8cd6ef1899fc7f7b1d7297131842a901
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-10-25 01:47:08 +0530

    auth is added

---
+++ ProjectLocation/location_finder/lib/app_screens/auth.dart
@@ -0,0 +1,34 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  
+  //sign in anon
+  Future signInAnon() async{
+    try{
+      AuthResult result = await _auth.signInAnonymously();
+      FirebaseUser user=result.user;
+      return user;
+    }catch (e){
+      print(e.toString());
+      return null;
+    }
+  }
+
+
+  Future<bool> signInWithEmail(String email, String password) async {
+    try {
+      AuthResult result = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      FirebaseUser user = result.user;
+      if (user != null) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (e) {
+      return false;
+    }
+  }
+}
